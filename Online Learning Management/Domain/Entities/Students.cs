a92958d4ebce3e1d9c5bd8c4d170ee66138500ad
armanonum <armandonunez404@gmail.com>
2024-06-11 15:12:55 -0400

    first commit

---
+++ Online Learning Management/Domain/Entities/Students.cs
@@ -0,0 +1,11 @@
+namespace Online_Learning_Management.Domain.Entities
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string Subject { get; set; }
+    }
+}
