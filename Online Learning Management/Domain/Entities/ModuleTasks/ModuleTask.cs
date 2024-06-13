9606a687ca44857e6714c1335dd4398a24de1fc8
Fernando <vluisfernando62@gmail.com>
2024-06-13 17:07:08 -0400

    add the moduleTask entity and repository to generate the migration

---
+++ Online Learning Management/Domain/Entities/ModuleTasks/ModuleTask.cs
@@ -0,0 +1,10 @@
+﻿namespace Online_Learning_Management.Domain.Entities.ModuleTasks
+{
+    public class ModuleTask
+    {
+        public Guid Id { get; set; }
+        public string? ModuleID { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
--- Online Learning Management/Infrastructure/Data/ApplicationDbContext.cs
+++ Online Learning Management/Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Online_Learning_Management.Domain.Entities;
+using Online_Learning_Management.Domain.Entities.ModuleTasks;
+using Online_Learning_Management.Infrastructure.Repositories.ModuleTasks;
 
 namespace Online_Learning_Management.Infrastructure.Data
 {
@@ -9,5 +11,13 @@ namespace Online_Learning_Management.Infrastructure.Data
         {
         }
         public DbSet<User> Users { get; set; }
+        public DbSet<ModuleTask> ModuleTasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new ModuleTaskConfiguration());
+        }
     }
 }
