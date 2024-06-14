97bdad56c82010fedc77ea673263f641c93591a2
NicoZela <fezelayani@gmail.com>
2024-06-13 22:22:45 -0400

    fix(modules): final build to ensure project error fixed throug dependency injection

--- Online Learning Management/Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
+++ Online Learning Management/Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using AutoMapper;
 using System.Reflection;
-using Online_Learning_Management.Domain.Interfaces;
 using Online_Learning_Management.Infrastructure.Repositories.Modules;
-using Online_Learning_Management.Infrastructure.Repositories;
 using Online_Learning_Management.Domain.Interfaces.Modules;
 using Online_Learning_Management.Application.Modules.Services;
 
@@ -13,7 +11,6 @@ namespace Online_Learning_Management.Infrastructure.DependencyInjection
         public static void AddProjectServices(this IServiceCollection services)
         {
             //Register repositories
-            services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IModuleRepository, ModuleRepository>();
 
             //Register services
