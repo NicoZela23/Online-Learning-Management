d57601ab727e7449fccf187ad1348548dff4c56d
Fernando <vluisfernando62@gmail.com>
2024-06-20 19:41:35 -0400

    Modifying endpoint responses.

--- Online Learning Management/Domain/Interfaces/ModuleTasks/IModuleTaskRepository.cs
+++ Online Learning Management/Domain/Interfaces/ModuleTasks/IModuleTaskRepository.cs
@@ -4,9 +4,9 @@ namespace Online_Learning_Management.Domain.Interfaces.ModuleTasks
 {
     public interface IModuleTaskRepository
     {
-        Task<IEnumerable<ModuleTask>> GetAllTasksOfModuleAsync();
+        Task<IEnumerable<ModuleTask>> GetAllTasksOfModuleAsync(Guid moduleID);
         Task<ModuleTask> GetTaskOfModuleByIdAsync(Guid id);
-        Task AddTaskToModuleAsync(ModuleTask moduleTask);
+        Task<ModuleTask> AddTaskToModuleAsync(ModuleTask moduleTask);
         Task UpdateTaskOfModuleAsync(ModuleTask moduleTask);
         Task DeleteTaskOfModuleAsync(Guid id);
     }
