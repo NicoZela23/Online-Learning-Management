cbfc8db3681684031d6d7ac68cfc84fb47d8c362
Fernando <vluisfernando62@gmail.com>
2024-06-24 16:13:43 -0400

    Student assignment upload ready

---
+++ Online Learning Management/Application/TaskStudent/Responses/RelationshipDataStructure.cs
@@ -0,0 +1,9 @@
+﻿namespace Online_Learning_Management.Application.TaskStudent.Responses
+{
+    public class RelationshipDataStructure
+    {
+        public string TaskTitle { get; set; }
+        public string StudentName { get; set; }
+        public string FileUrl {  get; set; }
+    }
+}
---
+++ Online Learning Management/Application/TaskStudent/Validator/CreateTaskStudentValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Online_Learning_Management.Infrastructure.DTOs.TaskStudent;
+
+namespace Online_Learning_Management.Application.TaskStudent.Validator
+{
+    public class CreateTaskStudentValidator : AbstractValidator<CreateTaskStudentDTO>
+    {
+        public CreateTaskStudentValidator()
+        {
+            RuleFor(x => x.ModuleTaskID).NotEmpty()
+               .WithMessage("ModuleTaskID is required.");
+            RuleFor(x => x.StudentID).NotEmpty()
+                .WithMessage("StudentID is required.");
+        }
+    }
+}
